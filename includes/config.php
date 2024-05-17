1f77981fd9e3147a21c1fc4d140328d2c5b2a47d
Francisco Javier <105751133+Javiervaz10@users.noreply.github.com>
2024-05-16 23:06:11 -0600

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,12 @@
 <?php
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "industria";
-
+*/
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "proyecto-final-db.mysql.database.azure.com", "u20051268", "{userL20051268-}", "{industria}", 3306, MYSQLI_CLIENT_SSL);
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
